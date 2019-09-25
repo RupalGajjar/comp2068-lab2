d6ebd228cf068cde8c8e1fb685947166b4d2fbe8
Rupal <200393408@student.georgianc.on.ca>
2019-09-24 21:22:03 -0400

    Final

--- ExpressApp1/app.js
+++ ExpressApp1/app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var lab2 = require('./routes/lab2');
 var app = express();
 
 // view engine setup
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/lab2', lab2);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
